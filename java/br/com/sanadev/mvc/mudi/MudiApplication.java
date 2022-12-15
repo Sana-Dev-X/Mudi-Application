a85a1861c6d1715f877961c3289b8ee0c9db19c0
Deiverson Amorim <deiver.sanamori@gmail.com>
2022-12-15 17:52:37 -0300

    login implementado

--- java/br/com/sanadev/mvc/mudi/MudiApplication.java
+++ java/br/com/sanadev/mvc/mudi/MudiApplication.java
@@ -3,9 +3,11 @@ package br.com.sanadev.mvc.mudi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan("br.com.sanadev.mvc.mudi.model")
+@ComponentScan("br.com.sanadev.mvc.mudi")
 public class MudiApplication {
 
 	public static void main(String[] args) {
---
+++ java/br/com/sanadev/mvc/mudi/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package br.com.sanadev.mvc.mudi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+		//.authorizeHttpRequests().anyRequest().authenticated().and()
+		.authorizeHttpRequests(aut->{aut.anyRequest().authenticated();})
+		
+		.formLogin().loginPage("/login").permitAll();
+		
+		 
+     return http.build();
+	}
+
+	@Bean(name = "userDetails")
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder().username("sana").password("1234").roles("ADM").build();
+		return new InMemoryUserDetailsManager(user);
+
+	}
+
+}
